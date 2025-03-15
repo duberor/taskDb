816afe981f0ec7e259340ca7363137f5d0f5afe6
Dub Eror <115782805+duberor@users.noreply.github.com>
2025-03-15 23:10:31 +0000

    Змінив порт на 5050

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('some.json');
+const router = jsonServer.router('some.json'); // Файл вашої бази даних
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
