d9dfa470ad7e14a076e237c429cc09c88e0db2fc
Dub Eror <115782805+duberor@users.noreply.github.com>
2025-03-16 02:31:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('some.json'); // Файл вашої бази даних
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(5050, () => {
-    console.log('JSON Server is running on port 5050');
+
+const PORT = process.env.PORT || 5050;
+server.listen(PORT, () => {
+  console.log(`JSON Server працює на порту ${PORT}`);
 });
